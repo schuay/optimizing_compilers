99da4659d7c78442705e94fae26fdf8772d68fe0
Jakob Gruber <jakob.gruber@gmail.com>
2014-11-24 12:06:20 +0100

    Handle multiplication by 0

--- hw2/constprop/full_example.c
+++ hw2/constprop/full_example.c
@@ -6,10 +6,18 @@ int main()
   z = x + y;
 
   y = 7;
-  z = x + y;
+  z = (x - y) * x / y;
 
   while (x < 10) {
     z = z + 1;
+  }
+
+  if (x <= 10) {
+    int x;
+    x = z * 0;
+  } else if (x == 11) {
+    int y;
+    y = x / 0;
   }
 
   return 0;
